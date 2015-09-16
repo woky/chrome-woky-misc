e134a06817b0d3c13429751adc898b71f5c2e5e1
woky <nechtom@gmail.com>
2015-09-16 22:59:51 +0200

    Don't set openerTabId for new tab
    The "Open link in new tab" action in the original tab put new tab after the tab
    created with Ctrl-T with openerTabId set.

--- background.js
+++ background.js
@@ -5,8 +5,7 @@ chrome.commands.onCommand.addListener(cmd => {
 		let q = {active: true, currentWindow: true};
 		chrome.tabs.query(q, tabs => {
 			chrome.tabs.create({
-				index: tabs[0].index + 1,
-				openerTabId: tabs[0].id // not sure about this
+				index: tabs[0].index + 1
 			});
 		});
 	}
